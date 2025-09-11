c646a210b4bd6dd7f0cf55b35c982c108e4bb09b
Oliver Huang <oliverbhuang@gmail.com>
2025-09-11 14:48:33 -0700

    add example workflow

---
+++ my_workflow.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+import os
+import pathlib
+import time
+
+import daft
+from daft.functions.ai import embed_text
+
+
+def mkdir() -> str:
+    desktop = os.path.join(pathlib.Path("~").expanduser(), "Desktop")
+    timestamp = str(int(time.time()))
+    path = os.path.join(desktop, timestamp)
+    pathlib.Path(path).mkdir(exist_ok=True, parents=True)
+    return path
+
+
+def my_workflow(name: str) -> dict:
+    df = daft.from_pydict(
+        {
+            "text": [
+                "Alice was beginning to get very tired of sitting by her sister on the bank.",
+                "So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid),",
+                "whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies,",
+                "when suddenly a White Rabbit with pink eyes ran close by her.",
+                "There was nothing so very remarkable in that;",
+                "nor did Alice think it so very much out of the way to hear the Rabbit say to itself, 'Oh dear! Oh dear! I shall be late!'",
+            ]
+        }
+    )
+
+    results = {}
+    for i in range(3):
+        dest = mkdir()
+        df_with_emb = df.with_column("embedding", embed_text(df["text"]))
+        df_with_emb = df_with_emb.write_parquet(dest)
+        results[f"run_{i + 1}"] = dest
+
+    return {"results": results}
